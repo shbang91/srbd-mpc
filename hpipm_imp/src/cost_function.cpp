ae1cba1ba5af29edd6aa8130b35ca6411693260f
Maxx Wilson <jessemaxxwilson@utexas.edu>
2022-07-01 11:53:16 -0500

    Cost Function Documentation

--- hpipm_imp/src/cost_function.cpp
+++ hpipm_imp/src/cost_function.cpp
@@ -36,9 +36,13 @@ void CostFunction::setQP(const ContactSchedule& contact_schedule,
                          const GaitCommand& gait_command, QPData& qp_data) {
   v_command_.template head<3>() = gait_command.vcom;
   v_command_.template tail<3>() << 0., 0., gait_command.yaw_rate;
-  dq_command_ = dt_ * v_command_;
+  
   base_pose_.template head<3>() = robot_state.com();
   base_pose_.template tail<4>() = robot_state.quat().coeffs();
+  
+  // Change in COM Position between MPC nodes
+  dq_command_ = dt_ * v_command_;
+
   // The reference rotation whose pitch and roll angles are 0.
   const double cref = robot_state.R().coeff(0, 0);
   const double sref = robot_state.R().coeff(1, 0);
@@ -49,12 +53,17 @@ void CostFunction::setQP(const ContactSchedule& contact_schedule,
                         0.,              0., 1.;
   base_pose_ref_[0].template head<3>() = robot_state.com();
   base_pose_ref_[0].template tail<4>() = Quaterniond(R_ref).coeffs();
+
+  // Integrate velocity command and current pose to get expected COM trajectory
+  // Populates base_pose_ref vector
   for (int i=0; i<qp_data.dim.N; ++i) {
     single_rigid_body_.integrate(base_pose_ref_[i], dq_command_, base_pose_ref_[i+1]);
   }
+
   // qdiff(q, qref)^T Q qdiff(q, qref)
   // -> linearize: (qdiff(q, qref) + Jqdiff_dqf(q, qref) dq) ^T Q (qdiff(q, qref) + Jqdiff_dqf(q, qref) dq)
   for (int i=0; i<qp_data.dim.N+1; ++i) {
+    // Get error linearized around base_pose_ref
     single_rigid_body_.difference(base_pose_, base_pose_ref_[i], qdiff_);
     single_rigid_body_.dDifference_dqf(base_pose_, base_pose_ref_[i], Jqdiff_);
     JtQqq_.noalias() = Jqdiff_.transpose() * Qqq_;
